803b363871dc7003be03bdcf2a307fdf6c0a3a9b
Mike Chevett <mchevett@gmail.com>
2013-05-25 00:39:29 -0400

    handle embedded images
    - data:image

--- rewrite/css.js
+++ rewrite/css.js
@@ -1,5 +1,8 @@
 module.exports = function(css, urlRewriter) {
     return css.replace(/url\s*\(\s*('|")?(.+?)('|")?\s*\)/gi, function(a,b,c,d){
+        if (c.match(/^data:image/i))
+            return a;
+
         return 'url(' + (b || '') + urlRewriter(c) + (d || '') +')';
     });
 }

a20eed1c809f7d5e34282849bfe47b5844375214
Mike Chevett <mchevett@gmail.com>
2013-06-01 23:12:20 -0400

    added handlebars middleware

--- app.js
+++ app.js
@@ -6,7 +6,6 @@
 var express = require('express')
   , routes = require('./routes')
   , proxy = require('./proxy')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , settings = require('./settings')()
@@ -18,17 +17,19 @@ var app = express();
 app.set('port', process.env.PORT || settings.port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
-app.set('domain', 'localhost');
+
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use('/templates.js', require('connect-handlebars')( __dirname + '/public/templates', { ext: ['handlebars'] }))
+app.use(app.router);
 
 app.locals({
-    LastCommit: process.env.MT3_lastCommit
+    LastCommit: process.env.MT3_lastCommit || '2dd0af47bc8586681b48733ec8f27413d0489e6a'
 });
 
 
@@ -40,7 +41,7 @@ if ('development' == app.get('NODE_ENV')) {
 app.get('/', routes.index);
 app.get('/*', proxy.go);
 
-app.get('/users', user.list);
+
 
 app.use(function(err, req, res, next){
     console.error(err.stack);

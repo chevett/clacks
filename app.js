3d4634bce7547d9a2ce4a9385e44b1d2546771fb
Mike Chevett <mchevett@gmail.com>
2013-06-03 14:05:01 -0400

    cleanup app.js a bit

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
   , proxy = require('./proxy')
   , http = require('http')
-  , fs = require('fs')
   , path = require('path')
   , settings = require('./settings')()
 ;
 
 var app = express();
 
+
+
+// settings
 app.locals({
     LastCommit: process.env.MT3_lastCommit || '2dd0af47bc8586681b48733ec8f27413d0489e6a'
 });
-
-// all environments
 app.set('port', process.env.PORT || settings.port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
 
 
+// middleware
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -31,23 +28,20 @@ app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(app.router);
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(500, 'Something broke!');
+});
 
+
+// routing
 app.get('/', routes.home);
 app.get('/scripts/handlebars.js', routes.handlebars);
 app.get('/scripts/templates.js', routes.templates);
 app.get('/*', proxy.go);
 
 
-
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.send(500, 'Something broke!');
-});
-
+// start
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-
-exports.settings = settings;

69976bed3176784b3a18ff9091c87a501249f4f6
Mike Chevett <mchevett@gmail.com>
2013-06-05 15:29:53 -0400

    kill some dead code

--- app.js
+++ app.js
@@ -19,40 +19,8 @@ app.set('view engine', 'hjs');
 // middleware
 app.use(express.favicon());
 app.use(express.logger('dev'));
-//app.use(express.bodyParser());
-//app.use(express.methodOverride());
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-/*app.use(function(req, res, next){
-    // just hack in some middleware to grab the request body.  i think proxy.js should be middleware itself.
-
-    var buf = '';
-
-    req.mt3 = req.mt3 || {};
-
-
-    req.on('data', function(chunk){
-        buf += chunk
-        console.log('data received: ' +chunk)
-
-    });
-
-    req.on('end', function(){
-
-        console.log('end: ')
-
-        req.mt3.body = buf;
-    });
-
-    console.log('mt3 body grabber calling next');
-
-    // this feels wrong
-    next();
-
-});*/
-
 app.use(app.router);
 app.use(function(err, req, res, next){
     console.error(err.stack);

cbf652f84bba230da3427bbe1dd7c35427fccf30
Mike Chevett <mchevett@gmail.com>
2013-06-06 13:27:02 -0400

    small fixes in urlhelper

--- url-helper.js
+++ url-helper.js
@@ -11,12 +11,7 @@ function _isSecure(url){
 }
 
 function _isRequestSecure(req){
-    if (settings.isProduction){
-        return req.headers['x-forwarded-proto'] == 'https';
-    }
-    else {
-        return req.secure;
-    }
+    return settings.isProduction? req.headers['x-forwarded-proto'] == 'https' : req.secure;
 }
 
 
@@ -27,7 +22,7 @@ function _getTargetUrl(request){
 
     myUrl = (isSecure ? 'https://' :  'http://') + myUrl;
 
-    o = url.parse(myUrl)
+    o = url.parse(myUrl);
     o.port = isSecure ? settings.sslPort : settings.port;
 
     return url.format(o);
@@ -39,11 +34,12 @@ exports.createProxyUrlRewriter = function(request){
     return function (originalUrl){
         var o = Object.create(settings);
 
-        // we always return absolute urls, so use the current request to resolve any relative urls.
         if (_isRelative(originalUrl)){
+
+            // we always return absolute urls, so use the current request to resolve any relative urls.
             originalUrl = url.resolve(requestTargetUrl, originalUrl);
 
-            if (request.isSecure){
+            if (_isRequestSecure(request)){
                 o.protocol = 'https:';
                 o.port = settings.sslPort;
             }

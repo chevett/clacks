deca70df5da6d35431015bdcca94db5c75425767
Mike Chevett <mchevett@gmail.com>
2013-06-23 18:13:11 -0400

    disable content security policy for now

--- rewrite/response/headers/index.js
+++ rewrite/response/headers/index.js
@@ -1,2 +1,11 @@
+var o = {};
+function _deleteHeader(){return null;}
 
-module.exports = require('../../header-converter').create(__dirname);
+module.exports = require('../../header-converter').create(__dirname, o);
+
+
+// just kill it for now.  should rewrite this later.
+// http://en.wikipedia.org/wiki/Content_Security_Policy
+o['content-security-policy'] = _deleteHeader;
+o['x-webkit-csp'] = _deleteHeader;
+o['x-forwarded-for'] = _deleteHeader;

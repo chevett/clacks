8c7dd0dbecb6b5bf9c126be1164dbd951bf8b1ea
Mike Chevett <mchevett@appnexus.com>
2013-08-27 20:29:12 -0400

    use the test helper in host.spec

--- rewrite/request/headers/host.spec.js
+++ rewrite/request/headers/host.spec.js
@@ -1,33 +1,20 @@
 var urlHelper = require('../../../url-helper');
 var settings = require('../../../settings')();
+var testHelper = require('../../../test/helper');
 var hostHeaderRewriter = require('./host');
 var assert = require('assert');
 
 
 describe('host request header', function(){
 	it('should convert to the real host', function  (){
-		var toProxyUrlFn = urlHelper.createToProxyUrlFn({
-			url: '/www.google.com',
-			secure: true, 
-			headers: {
-				'x-forwarded-proto': 'https'
-			}
-		});
-		
+		var toProxyUrlFn = urlHelper.createToProxyUrlFn(testHelper.createSecureRequest('/www.google.com'));
 		var targetHost = hostHeaderRewriter('this value does not matter', toProxyUrlFn);
 		
 		assert(targetHost, 'www.google.com');
 	});
 
 	it('should convert to the real host when a protocol override is used', function  (){
-		var toProxyUrlFn = urlHelper.createToProxyUrlFn({
-			url: '/http/www.google.com',
-			secure: true, 
-			headers: {
-				'x-forwarded-proto': 'https'
-			}
-		});
-		
+		var toProxyUrlFn = urlHelper.createToProxyUrlFn(testHelper.createSecureRequest('/www.google.com'));
 		var targetHost = hostHeaderRewriter('this value does not matter', toProxyUrlFn);
 		
 		assert(targetHost, 'www.google.com');

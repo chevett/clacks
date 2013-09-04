e872de22507a6893b1aea7afbf4552a089680679
Mike Chevett <mchevett@appnexus.com>
2013-09-04 19:49:44 -0400

    don't read files every function call

--- injectors/index.js
+++ injectors/index.js
@@ -1,16 +1,16 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 
+var injectors = [];
+
+fs.readdirSync(__dirname).forEach(function(file) {
+	if (/\.js$/i.test(file) && file!='index.js' && !/\.spec\.js$/i.test(file)) {
+		injectors.push(require(__dirname + '/' + file ));
+	}
+});
+
 module.exports = function(context, data) {
-	var injectors = [];
-
-    fs.readdirSync(__dirname).forEach(function(file) {
-        if (/\.js$/i.test(file) && file!='index.js' && !/\.spec\.js$/i.test(file)) {
-            injectors.push(require(__dirname + '/' + file ));
-        }
-    });
-
-    var $ = cheerio.load(data.body) ;
+    var $ = cheerio.load(data.body);
 
 	for (var i=0; i< injectors.length; i++){
 		if (injectors[i]($, context, data) === false) break;

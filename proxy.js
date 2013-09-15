3f1e2709407e8e895cf04af545d4bf32c97e57a2
Mike Chevett <mchevett@appnexus.com>
2013-09-14 22:02:56 -0400

    put the connect function back in it's own file

---
+++ proxy.js
@@ -0,0 +1,42 @@
+var injectors =  require('./injectors/'),
+	Context = require('./context/'),
+	TranslatedRequest = require('./translated-request');
+
+module.exports = function(req, res){
+	var headersModel = {};
+	var ctx = new Context(req, res);
+
+	if (!ctx.target){
+		res.writeHead(400, {});
+		res.write('<h1>400</h1>');
+		res.write(req.url.substr(1) + ' is a bad request');
+		return;
+	}
+
+	var request = new TranslatedRequest(ctx, {
+		url: ctx.target.url,
+		method: req.method,
+		headers: req.headers
+	});
+
+	request.on('headers', function(headers){
+		headersModel.request = headers;
+	});
+
+	req.pipe(request);
+
+	request.on('ready', function(response){
+		response.on('headers', function(statusCode, headers){
+			res.writeHead(statusCode, headers.toObject());
+			headersModel.response = headers;
+		});
+
+		response.on('before-write', function(data){
+			if (data.contentType==='text/html'){
+				data.body = injectors(ctx, {body: data.body, headers: headersModel});
+			}
+		});
+
+		response.pipe(res);
+	});
+};

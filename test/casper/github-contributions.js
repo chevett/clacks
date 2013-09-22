931981270a95e48c35faf682091ccf018a3157a0
Mike Chevett <mchevett@appnexus.com>
2013-09-22 11:43:03 -0400

    call test.pass/fail
    - this won't run against localhost for some reason

--- test/casper/github-contributions.js
+++ test/casper/github-contributions.js
@@ -1,12 +1,15 @@
 /* global casper: false */
 
 casper.test.begin('githib contributions graph loads', function(test){
-	casper.start('https://www.miketown3.com/https://github.com/chevett', function(){
+	casper.start('http://www.miketown3.com/http://github.com/chevett', function(){
 		this.echo(this.getTitle());
 	});
 
 	casper.waitForSelector('.calendar-graph svg g g', function(){
 		this.captureSelector('./github-contributions.png', 'html');
+		this.test.pass('contributions graph found.');
+	}, function(){
+		this.test.fail('contributes graph NOT found.');
 	});
 
 	casper.run(function(){

6cdd0984c172b1b4406e1f4638e1b828759da384
Mike Chevett <mchevett@appnexus.com>
2013-09-24 08:07:06 -0400

    just messing around with traverse

--- rewriters/response/js.js
+++ rewriters/response/js.js
@@ -1,39 +1,23 @@
 var esprima = require('esprima');
 var escodegen = require('escodegen');
+var traverse = require('traverse');
 
-function traverse(object, visitor) {
-	var key, child;
-
-	visitor.call(null, object);
-
-	for (key in object) {
-		if (!object.hasOwnProperty(key)) continue; 
-		child = object[key];
-		if (typeof child === 'object' && child !== null) {
-			traverse(child, visitor);
-		}
-	}
-}
 
 function _checkString(str, ctx){
-	try {
-		//if (/^http:\/\//i.test(str)) return ctx.server.url + str;
-		//if (/^https:\/\//i.test(str)) return ctx.server.secureUrl + str;
+	//if (/^http:\/\//i.test(str)) return ctx.server.url + str;
+	//if (/^https:\/\//i.test(str)) return ctx.server.secureUrl + str;
 
-		return str;
-	} catch (e){
-		console.log('error in js rewriter.');
-		console.log(str);
-		return str;
-	}
+	return str;
 }
 
 function _rewrite(js, ctx){
 	var tree = esprima.parse(js);
 
-	traverse(tree, function(node){
+	traverse(tree).forEach(function(node){
+		//console.log(node);
 		if (node.type === 'Literal' && typeof node.value === 'string'){
 			node.value = _checkString(node.value, ctx);
+			this.update(node, true);
 		}
 
 	});

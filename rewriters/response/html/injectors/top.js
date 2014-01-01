8b55199518b7abcf2392dcd3b86dda32fec56218
Mike Chevett <mchevett@appnexus.com>
2014-01-01 13:05:02 -0500

    always inject at the very top

--- rewriters/response/html/injectors/top.js
+++ rewriters/response/html/injectors/top.js
@@ -2,19 +2,11 @@ var _ = require('underscore');
 var m = require('merle');
 
 module.exports = function($, context){
-	var $body = $('head meta:last-child'),
+	var $head = $('head'),
 		viewModel = {
 			headers:{}
 		},
 	fnAppend;
-
-	if ($body && $body.length){
-		fnAppend = $body.append.bind($body);
-	} else {
-
-		$body = $('head');
-		fnAppend = $body.prepend.bind($body);
-	}
 
 	var allCalls = _.chain(context.convert.nostalgorithm.calls)
 		.filter(function(v){ return (/headers/).test(v.name);}) // need this because on google.com there are urls that break json encoding // need this because on google.com there are urls that break json encoding
@@ -26,8 +18,9 @@ module.exports = function($, context){
 			};
 		})
 		.value();
+
 	var $script = $('<script></script>')
 		.html('_mt3_= __mt3_ = function(v){return v;};console.log("running data");var mt3 = mt3 || {}; mt3.debug = '+ JSON.stringify(allCalls) + ';');
 
-	fnAppend($script);
+	$head.prepend($script);
 };
